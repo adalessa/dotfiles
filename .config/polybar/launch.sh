0f1ef02d5b1cc6091e621437aea64d831a051478
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2020-03-03 18:17:07 -0300

    add missing configs

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload mybar &
+#   done
+# else
+#   polybar --reload mybar &
+# fi
+
+polybar --reload mon1 &
+polybar --reload mon2 &
+
+
+echo "Polybar launched..."
