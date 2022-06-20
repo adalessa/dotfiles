8bb40990ffdeeedd6338e43565712d22e453fbe4
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2022-06-20 11:50:08 +0200

    Add layouts

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -69,11 +69,11 @@ local modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.tile,
-    -- awful.layout.suit.floating,
+    awful.layout.suit.fair,
+    awful.layout.suit.floating,
     -- awful.layout.suit.tile.left,
     -- awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
-    -- awful.layout.suit.fair,
     -- awful.layout.suit.fair.horizontal,
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
