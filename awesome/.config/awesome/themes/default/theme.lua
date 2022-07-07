e6edf88153e38ff5be588f16c7c52270d019f29c
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2022-07-07 22:40:45 +0200

    new wallpaper, fix input query

--- awesome/.config/awesome/themes/default/theme.lua
+++ awesome/.config/awesome/themes/default/theme.lua
@@ -101,7 +101,7 @@ theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/m
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
 -- theme.wallpaper = themes_path.."default/background.png"
-theme.wallpaper = themes_path.."default/wallpaper.png"
+theme.wallpaper = themes_path.."default/forest.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
