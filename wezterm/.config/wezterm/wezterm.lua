eb99e087824a61aced2947a3434d343a01007af4
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2022-08-26 17:39:44 +0200

    Adding plugin for rust annalizer, improve wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,27 +1,17 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
+
+wezterm.on("update-right-status", function(window)
+	window:set_right_status(window:active_workspace())
+end)
 
 return {
-	font = wezterm.font("JetBrains Mono"),
-    font_size = 13,
-    use_resize_increments = true,
-    line_height = 1.0,
-	color_scheme = "nightfox",
+	-- General configuration
 	audible_bell = "Disabled",
-	window_background_image = wezterm.home_dir .. "/.config/wezterm/term_wallpaper.png",
-	window_background_image_hsb = {
-		brightness = 0.1,
-
-		-- You can adjust the hue by scaling its value.
-		-- a multiplier of 1.0 leaves the value unchanged.
-		hue = 0.65,
-
-		-- You can adjust the saturation also.
-		saturation = 0.3,
-	},
-	use_fancy_tab_bar = false,
 	window_close_confirmation = "NeverPrompt",
-	enable_tab_bar = false,
-	default_prog = { "tmux", "new-session", "-A", "-s", "Main" },
+	use_fancy_tab_bar = false,
+	enable_tab_bar = true,
+
 	window_padding = {
 		left = 2,
 		right = 2,
@@ -29,11 +19,28 @@ return {
 		bottom = 2,
 	},
 
+	-- Font and color scheme
+	font = wezterm.font("JetBrains Mono"),
+	font_size = 13,
+	use_resize_increments = true,
+	line_height = 1.0,
+	color_scheme = "nightfox",
+
+	-- Background
+	window_background_image = wezterm.home_dir .. "/.config/wezterm/term_wallpaper.png",
+	window_background_image_hsb = {
+		brightness = 0.1,
+		hue = 0.65,
+		saturation = 0.3,
+	},
+
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
 	keys = {
-		{ key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
+		{ key = "=", mods = "SUPER", action = wezterm.action.IncreaseFontSize },
+		{ key = "-", mods = "SUPER", action = wezterm.action.DecreaseFontSize },
 		{
-			key = "P",
-			mods = "CTRL",
+			key = "m",
+			mods = "LEADER",
 			action = wezterm.action.QuickSelectArgs({
 				label = "open url",
 				patterns = {
@@ -47,9 +54,68 @@ return {
 			}),
 		},
 		{
-			key = "Z",
-			mods = "CTRL",
-			action = wezterm.action.TogglePaneZoomState,
+			key = "9",
+			mods = "ALT",
+			action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
+		},
+		{ key = "n", mods = "LEADER|CTRL", action = act.SwitchWorkspaceRelative(1) },
+		{ key = "p", mods = "LEADER|CTRL", action = act.SwitchWorkspaceRelative(-1) },
+		{
+			key = "u",
+			mods = "LEADER|CTRL",
+			action = wezterm.action_callback(function(win, pane)
+				-- here what I want have a project set or look for it
+				-- so need to interact with workspaces
+				--
+				-- so need a child process that runs fzf base on the list and return the name and directory
+				-- return as json {"name": "some name", "directory": "abs path", "group": "group"}
+				-- not sure what to do do with group yet but better to have it
+				-- local success, stdout, stderr = wezterm.run_child_process { 'ls', '-l' }
+				-- act.SwitchToWorkspace {
+				-- name = 'name',
+				-- spawn = {
+				--   cwd = 'path of the project',
+				--   args = { 'nvim' },
+				-- },
+				-- },
+				-- very important this how to sync with other funciton perform action from window
+				-- win:perform_action(act.SwitchToWorkspace({
+				-- 	name = "some test",
+				-- 	spawn = {
+				-- 		cwd = "/home/adalessa/code/php/carin-gateway-service/",
+				-- 		args = { "nvim" },
+				-- 	},
+				-- }), pane)
+			end),
+		},
+		{ key = "n", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+		{ key = "\\", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+		{ key = "a", mods = "LEADER|CTRL", action = act.ActivateLastTab },
+		{ key = "1", mods = "LEADER", action = act.ActivateTab(0) },
+		{ key = "2", mods = "LEADER", action = act.ActivateTab(1) },
+		{ key = "3", mods = "LEADER", action = act.ActivateTab(2) },
+
+		{
+			key = "h",
+			mods = "LEADER",
+			action = act.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "l",
+			mods = "LEADER",
+			action = act.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "k",
+			mods = "LEADER",
+			action = act.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "j",
+			mods = "LEADER",
+			action = act.ActivatePaneDirection("Down"),
 		},
 	},
 }
