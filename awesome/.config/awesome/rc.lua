6be8b1e00c92833e70c0b292b31171da43c555c3
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2022-09-30 19:18:07 +0200

    small changes

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -274,9 +274,9 @@ end)
 root.buttons(gears.table.join(
     awful.button({}, 3, function()
         mymainmenu:toggle()
-    end),
-    awful.button({}, 4, awful.tag.viewnext),
-    awful.button({}, 5, awful.tag.viewprev)
+    end)
+    -- awful.button({}, 4, awful.tag.viewnext),
+    -- awful.button({}, 5, awful.tag.viewprev)
 ))
 -- }}}
 
