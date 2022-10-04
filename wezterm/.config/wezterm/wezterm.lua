33fe605c1009d7a01e8adadee33aafd89c01121e
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2022-10-04 08:44:32 +0200

    feat: small changes and improvementes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -30,14 +30,14 @@ return {
 		-- Select a fancy italic font for italic text
 		{
 			italic = true,
-			font = font_with_fallback("JetBrainsMono Nerd Font", { weight = 'Light', italic = true }),
+			font = font_with_fallback("JetBrainsMono Nerd Font", { weight = "Light", italic = true }),
 		},
 
 		-- Similarly, a fancy bold+italic font
 		{
 			italic = true,
 			intensity = "Bold",
-			font = font_with_fallback("JetBrainsMono Nerd Font", { weight = 'ExtraBold', italic = true }),
+			font = font_with_fallback("JetBrainsMono Nerd Font", { weight = "ExtraBold", italic = true }),
 		},
 	},
 	font_size = 13,
