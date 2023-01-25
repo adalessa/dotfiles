0a3969523b2fd504432e73a7fd79a47551d0a53b
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2023-01-25 16:17:17 +0100

    add home manager, spells and lazygit

---
+++ nix/.config/nixpkgs/home.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "adalessa";
+  home.homeDirectory = "/home/adalessa";
+
+  home.packages = [
+    pkgs.lazygit
+    pkgs.gpick
+    pkgs.insomnia
+    pkgs.lsd
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
